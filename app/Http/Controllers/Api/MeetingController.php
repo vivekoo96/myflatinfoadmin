f59b1b90ef2bb14f23d58b98ea0ec72cbb0e113c
vivek patel <vvivek814@gmail.com>
2026-04-13 22:06:56 +0530

    error fixed

--- app/Http/Controllers/Api/MeetingController.php
+++ app/Http/Controllers/Api/MeetingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Meeting;
+use App\Models\MeetingMinute;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 
