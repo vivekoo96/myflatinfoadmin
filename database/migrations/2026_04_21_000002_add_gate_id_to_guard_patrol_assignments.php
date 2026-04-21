ca3e13593b7b2f3f7ab8bc9dd638b877bb94a3ab
vivek patel <vvivek814@gmail.com>
2026-04-21 18:29:17 +0530

    migration issse fixed

--- database/migrations/2026_04_21_000002_add_gate_id_to_guard_patrol_assignments.php
+++ database/migrations/2026_04_21_000002_add_gate_id_to_guard_patrol_assignments.php
@@ -3,12 +3,16 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class AddGateIdToGuardPatrolAssignments extends Migration
 {
     public function up()
     {
         Schema::table('guard_patrol_assignments', function (Blueprint $table) {
+            // Make patrol_location_id nullable since we're moving to gate_id
+            DB::statement('ALTER TABLE guard_patrol_assignments MODIFY patrol_location_id BIGINT UNSIGNED NULL');
+
             $table->unsignedBigInteger('gate_id')->nullable()->after('building_shift_id');
             $table->index('gate_id');
         });
