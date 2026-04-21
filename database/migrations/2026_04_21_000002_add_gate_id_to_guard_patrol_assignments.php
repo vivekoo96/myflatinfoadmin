4f12ac84b1fa170daf91d1344cf9f0330c364e4f
vivek patel <vvivek814@gmail.com>
2026-04-21 18:28:04 +0530

    migration isse

--- database/migrations/2026_04_21_000002_add_gate_id_to_guard_patrol_assignments.php
+++ database/migrations/2026_04_21_000002_add_gate_id_to_guard_patrol_assignments.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateGuardPatrolAssignmentsAddGate extends Migration
+class AddGateIdToGuardPatrolAssignments extends Migration
 {
     public function up()
     {
         Schema::table('guard_patrol_assignments', function (Blueprint $table) {
             $table->unsignedBigInteger('gate_id')->nullable()->after('building_shift_id');
             $table->index('gate_id');
-            $table->unsignedBigInteger('patrol_location_id')->nullable()->change();
         });
     }
 
